b366910e596e8ea6e177f8d99d3d23ef89c1ce81
Chefen2003 <Knoes99@gmail.com>
2026-06-18 20:03:41 +0200

    added build command for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://simonnj.github.io",
-  base: "/HemaOdense",
+  site: "https://hemaodense.dk",
   output: "static",
 });
